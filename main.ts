69c8f72cc2ca997de779f8173c5befc8ecd92791
andrea pagliari <andrea.pagliari12@gmail.com>
2023-02-26 22:29:56 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -8,7 +8,7 @@ basic.forever(function () {
     mecanumRobot.Motor(LR.Upper_left, MD.Back, speed)
     mecanumRobot.Motor(LR.Lower_left, MD.Back, speed)
     mecanumRobot.Motor(LR.Upper_right, MD.Back, speed)
-    mecanumRobot.Motor(LR.Lower_right, MD.Forward, speed)
+    mecanumRobot.Motor(LR.Lower_right, MD.Back, speed)
     basic.pause(2000)
     mecanumRobot.Motor(LR.Upper_left, MD.Back, speed)
     mecanumRobot.Motor(LR.Lower_left, MD.Back, speed)
